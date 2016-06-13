4820e79f8d3bfda25df2a99617cc20a80a332355
Yanael Barbier <ybarbier@student.42.fr>
2016-06-13 15:04:03 +0200

    Change function ft_test_sign and add test

--- d00/ex00/ft_test_sign.ml
+++ d00/ex00/ft_test_sign.ml
@@ -1,5 +1,13 @@
-let ft_test_sign =
-    let n = read_int () in
+let ft_test_sign n =
     if n < 0
     then print_endline "negative"
     else print_endline "positive"
+
+
+let () = 
+    print_string "Test with [42]: ";
+    ft_test_sign 42;
+    print_string "Test with [0]: ";
+    ft_test_sign 0;
+    print_string "Test with [-42]: ";
+    ft_test_sign (-42);
