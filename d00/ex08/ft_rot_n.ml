55307bfbc0163d37223557a4d1bcb6493c472563
Yanael Barbier <st3w4r@gmail.com>
2016-06-13 23:52:19 +0200

    Send ft_rol_n

--- d00/ex08/ft_rot_n.ml
+++ d00/ex08/ft_rot_n.ml
@@ -1,21 +1,22 @@
 let ft_rot_n n str =
     let rot c =
-        let ascii_of_a = int_of_char 'a' in
-        let ascii_of_z = int_of_char 'z' in
-        let ascii_of_A = int_of_char 'A' in
-        let ascii_of_Z = int_of_char 'Z' in
-        let rec add_to_char c =
-            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-            then
-                char_of_int(int_of_char(c) + (n mod 26))
-            else c
-        in add_to_char c
+        if (c >= 'a' && c <= 'z')
+		then char_of_int(((((int_of_char c) - int_of_char 'a') + n) mod 26) + int_of_char 'a')
+		else
+			if (c >= 'A' && c <= 'Z')
+			then char_of_int(((((int_of_char c) - int_of_char 'A') + n) mod 26) + int_of_char 'A')
+        else c
     in String.map rot str
-
-(*let rot c = char_of_int (int_of_char (c) + (n mod 26))*)
 
 
 let main () =
-    print_endline (ft_rot_n 3 "Aello{")
+    print_endline (ft_rot_n 3 "AellWXxyz{");
+    print_endline (ft_rot_n 1 "abcdefghijklmnopqrstuvwxyz");
+    print_endline (ft_rot_n 13 "abcdefghijklmnopqrstuvwxyz");
+    print_endline (ft_rot_n 42 "0123456789");
+    print_endline (ft_rot_n 2 "OI2EAS67B9");
+    print_endline (ft_rot_n 0 "Damned !");
+    print_endline (ft_rot_n 42 "");
+    print_endline (ft_rot_n 1 "NBzlk qnbjr !")
 
 let () = main ()
