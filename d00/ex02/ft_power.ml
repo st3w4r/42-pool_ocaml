a99aeae00e3c771dd2efe4f5b65b269caf9ecc74
Yanael Barbier <ybarbier@student.42.fr>
2016-06-13 16:31:30 +0200

    Send ft_power When 0 0 is not solve

---
+++ d00/ex02/ft_power.ml
@@ -0,0 +1,32 @@
+let rec ft_power n p =
+    if (n <= 0 && p <= 0)
+    then 0
+    else
+    if (p <= 0)
+    then 1
+    else
+        begin
+        n * ft_power n (p-1)
+        end
+
+let () =
+    print_int (ft_power 2 4);
+    print_char '\n';
+    print_int (ft_power 3 0);
+    print_char '\n';
+    print_int (ft_power 0 5);
+    print_char '\n';
+    print_int (ft_power 2 5);
+    print_char '\n';
+    print_int (ft_power 5 10);
+    print_char '\n';
+    print_int (ft_power 5 2);
+    print_char '\n';
+    print_int (ft_power 6 3);
+    print_char '\n';
+    print_int (ft_power 0 1);
+    print_char '\n';
+    print_int (ft_power 1 0);
+    print_char '\n';
+    print_int (ft_power 0 0);
+    print_char '\n'
