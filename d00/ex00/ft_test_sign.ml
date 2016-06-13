abc1deb854c3f79e9f8e5f55a7bf90060728e222
Yanael Barbier <ybarbier@student.42.fr>
2016-06-13 15:08:07 +0200

    Remove print_string

--- d00/ex00/ft_test_sign.ml
+++ d00/ex00/ft_test_sign.ml
@@ -5,9 +5,6 @@ let ft_test_sign n =
 
 
 let () = 
-    print_string "Test with [42]: ";
     ft_test_sign 42;
-    print_string "Test with [0]: ";
     ft_test_sign 0;
-    print_string "Test with [-42]: ";
     ft_test_sign (-42);
