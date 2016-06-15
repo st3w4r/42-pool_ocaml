0fabe4da832c531fcc3c0baf94c3e97428ed0ee3
Yanael Barbier <ybarbier@student.42.fr>
2016-06-15 21:46:23 +0200

    d02 ex00 Identity and map_to_tuple

--- d02/ex00/encode.ml
+++ d02/ex00/encode.ml
@@ -6,30 +6,30 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/15 13:27:08 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/15 19:54:27 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/15 21:45:36 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 (*
 let is_same n = match n with
-                | (n0, n1) when n0 = n1 -> true
-                | (n0, _) -> false
+| (n0, n1) when n0 = n1 -> true
+| (n0, _) -> false
 
 
 let rec process_list i n lst =
-            if is_same ((List.nth lst i), (List.nth lst (i+1)))
+    if is_same ((List.nth lst i), (List.nth lst (i+1)))
             then (process_list (i+1) 1 lst)
             else (n, (List.nth lst i))
 *)
 
 (*
 let is_same n = match n with
-                | (n0, n1) when n0 = n1 -> true
-                | (n0, _) -> false
+| (n0, n1) when n0 = n1 -> true
+| (n0, _) -> false
 
 
 let rec process_list n lst =
-            if is_same ("a", "b")
+    if is_same ("a", "b")
             then process_list 2 lst
             else (n, "a")
 *)
@@ -38,13 +38,13 @@ let rec process_list n lst =
 
 (*
 let rec each_elem el0 el1 lst = match lst with
-    | [] -> (0, 0)
+| [] -> (0, 0)
     | head::next::queue -> (
         if head = next then 
-        else 0
+            else 0
     )
     | head::queue -> 0
-*)
+    *)
 
 (*
 let each_elem el0 el1 n =
@@ -56,18 +56,31 @@ let
 
 
 (*let rec map x lst = match lst with
-    | [] -> (1, x)
+| [] -> (1, x)
 *)
 
-let identity lst =
-    let rec revert lst acc = match lst with
+let rec identity lst acc = match lst with
         | [] -> acc
-        | head::queue -> iden queue (head::acc)
+        | head::queue -> identity queue (acc @ [head])
+     in identity lst []
+
+let rec map_to_tuple lst acc = match lst with
+    | [] -> acc
+    | head::queue -> map_to_tuple queue (acc @ [(1, head)])
+    in map_to_tuple lst []
+
+
+let map_to_tuple lst =
+    let rec revert lst acc = match lst with
+    | [] -> acc
+    | [a] -> [(1,a)]
+    | head::queue -> iden queue (head::acc)
     in revert (revert lst []) []
 
+
 let rec counter n x lst = match lst with
-    | [] -> 0
-    | tete::queue -> when tete = queue 
+| [] -> 0
+| tete::queue -> when tete = queue 
 
 let encode lst =
     iden (iden lst []) []
