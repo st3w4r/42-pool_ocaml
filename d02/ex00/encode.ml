5dc7dd9231f35c45b0351f9434d4c803f4bc54fc
Yanael Barbier <ybarbier@student.42.fr>
2016-06-15 19:55:39 +0200

    Identity list function

--- d02/ex00/encode.ml
+++ d02/ex00/encode.ml
@@ -6,10 +6,11 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/15 13:27:08 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/15 17:36:03 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/15 19:54:27 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
+(*
 let is_same n = match n with
                 | (n0, n1) when n0 = n1 -> true
                 | (n0, _) -> false
@@ -17,8 +18,9 @@ let is_same n = match n with
 
 let rec process_list i n lst =
             if is_same ((List.nth lst i), (List.nth lst (i+1)))
-            then (process_list (i+1) (i+1) lst)
+            then (process_list (i+1) 1 lst)
             else (n, (List.nth lst i))
+*)
 
 (*
 let is_same n = match n with
@@ -53,8 +55,22 @@ let
 *)
 
 
+(*let rec map x lst = match lst with
+    | [] -> (1, x)
+*)
+
+let identity lst =
+    let rec revert lst acc = match lst with
+        | [] -> acc
+        | head::queue -> iden queue (head::acc)
+    in revert (revert lst []) []
+
+let rec counter n x lst = match lst with
+    | [] -> 0
+    | tete::queue -> when tete = queue 
+
 let encode lst =
-    process_list 0 lst
+    iden (iden lst []) []
 
 let main () =
     encode [2; 2; 2; 2; 42; 42; 100; 101]
