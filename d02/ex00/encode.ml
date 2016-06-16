5f6faff2d6815096ca902bb30d44bdb1811d7289
Yanael Barbier <ybarbier@student.42.fr>
2016-06-16 13:55:53 +0200

    Semd d00 ex00 tests

--- d02/ex00/encode.ml
+++ d02/ex00/encode.ml
@@ -6,7 +6,7 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/15 13:27:08 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/16 13:42:32 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/16 13:55:22 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -19,7 +19,7 @@ let rec encode lst = match lst with
     )
     | [] -> []
 
-
+(*--INT--*)
 let rec print_list_int lst = match lst with
     | hd::tl -> (
         print_int hd;
@@ -54,15 +54,93 @@ let rec print_list_encoded_int lst = match lst with
     )
     | [] -> print_string "\n"
 
+(*--STRING--*)
+let rec print_list_string lst = match lst with
+    | hd::tl -> (
+        print_string hd;
+        print_string "; ";
+        print_list_string tl
+    )
+    | [] -> print_string "\n"
+
+let rec print_list_tuple_string lst = match lst with
+    | hd::tl -> (
+        let (n, x) = hd in
+        print_string "(";
+        print_int n;
+        print_string ", ";
+        print_string x;
+        print_string ")";
+        print_string "; ";
+        print_list_tuple_string tl
+    )
+    | [] -> print_string "\n"
+
+let rec print_list_encoded_string lst = match lst with
+    | hd::tl -> (
+        let (n, x) = hd in
+        let rec print_loop_string n x tl =
+        if n > 0 then (
+            print_string x;
+            print_loop_string (n-1) x tl
+        )
+        else print_list_encoded_string tl
+        in print_loop_string n x tl
+    )
+    | [] -> print_string "\n"
+
 
 let main () =
-    let list_int = [2; 2; 2; 2; 42; 42; 100; 101] in
+    (*--INT--*)
+    let list_int1 = [2; 2; 2; 2; 42; 42; 100; 101] in
     print_string "List: ";
-    print_list_int list_int;
+    print_list_int list_int1;
     print_string "Tuple: ";
-    print_list_tuple_int (encode list_int);
+    print_list_tuple_int (encode list_int1);
     print_string "Decode: ";
-    print_list_encoded_int (encode list_int)
+    print_list_encoded_int (encode list_int1);
+
+    let list_int2 = [2] in
+    print_string "List: ";
+    print_list_int list_int2;
+    print_string "Tuple: ";
+    print_list_tuple_int (encode list_int2);
+    print_string "Decode: ";
+    print_list_encoded_int (encode list_int2);
+
+    let list_int3 = [] in
+    print_string "List: ";
+    print_list_int list_int3;
+    print_string "Tuple: ";
+    print_list_tuple_int (encode list_int3);
+    print_string "Decode: ";
+    print_list_encoded_int (encode list_int3);
+
+    (*--STRING--*)
+    let list_string1 = ["a";"a";"a";"b";"b";"b"] in
+    print_string "List: ";
+    print_list_string list_string1;
+    print_string "Tuple: ";
+    print_list_tuple_string (encode list_string1);
+    print_string "Decode: ";
+    print_list_encoded_string (encode list_string1);
+
+    let list_string2 = ["OK"] in
+    print_string "List: ";
+    print_list_string list_string2;
+    print_string "Tuple: ";
+    print_list_tuple_string (encode list_string2);
+    print_string "Decode: ";
+    print_list_encoded_string (encode list_string2);
+
+    let list_string3 = ["Yay"; "Yay"; "YO";"Hey";"Hey"] in
+    print_string "List: ";
+    print_list_string list_string3;
+    print_string "Tuple: ";
+    print_list_tuple_string (encode list_string3);
+    print_string "Decode: ";
+    print_list_encoded_string (encode list_string3)
+
 
 let () = main ()
 
