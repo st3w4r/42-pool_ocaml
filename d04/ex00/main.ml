de3a00b2968ff07f1f9ef39d9acb2a0e62c3842a
Yanael Barbier <ybarbier@student.42.fr>
2016-06-17 17:33:13 +0200

    Refactoring test d04 ex00

--- d04/ex00/main.ml
+++ d04/ex00/main.ml
@@ -6,35 +6,20 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/17 14:39:33 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/17 16:37:03 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/17 17:32:34 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let main () =
     let rec print_all lst = match lst with
         | hd::tl -> (
-            let print_type lst = match lst with
-                | Color.Spade -> (print_string (Color.toString Color.Spade);
-                                  print_string " "; print_string
-                                  (Color.toStringVerbose Color.Spade);
-                                  print_endline "");
-                | Color.Heart -> (print_string (Color.toString Color.Heart);
-                                  print_string " "; print_string
-                                  (Color.toStringVerbose Color.Heart);
-                                  print_endline "");
-                | Color.Diamond -> (print_string (Color.toString Color.Diamond);
-                                    print_string " "; print_string
-                                    (Color.toStringVerbose Color.Diamond);
-                                    print_endline "");
-                | Color.Club -> (print_string (Color.toString Color.Club);
-                                 print_string " "; print_string
-                                 (Color.toStringVerbose Color.Club);
-                                 print_endline "")
-            in print_type hd;
-                print_all tl
+                print_string (Color.toString hd);
+                print_string " "; print_string
+                (Color.toStringVerbose hd);
+                print_endline "";
+            print_all tl
         )
         | [] -> ()
-
     in print_all Color.all
 
 let () = main ()
