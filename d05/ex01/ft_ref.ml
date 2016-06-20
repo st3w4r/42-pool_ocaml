81ced3fc84109702aa15f4c42585f4ae1e8ac477
Yanael Barbier <ybarbier@student.42.fr>
2016-06-20 20:07:34 +0200

    Add get set d05 ex00

--- d05/ex01/ft_ref.ml
+++ d05/ex01/ft_ref.ml
@@ -6,11 +6,16 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/20 17:42:14 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/20 18:46:10 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/20 20:07:21 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 type 'a ft_ref = {mutable contents:'a}
 
 let return x = {contents = x}
-    
+
+let get x = x.contents
+
+let set x y = x.contents <- y
+
+
