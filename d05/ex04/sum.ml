4642b325a6de25a324ae5cbc8cf526b20bb6400d
Yanael Barbier <ybarbier@student.42.fr>
2016-06-21 15:54:23 +0200

    Send d05 ex04 sum, test functions

--- d05/ex04/sum.ml
+++ d05/ex04/sum.ml
@@ -6,8 +6,14 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/21 11:07:08 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/21 11:13:12 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/21 15:53:44 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let sum = (+.)
+
+let () =
+    print_float (sum 2.0 2.2); print_endline "";
+    print_float (sum 1.0 (-1.2)); print_endline "";
+    print_float (sum 0.2 0.2); print_endline "";
+    print_float (sum 0.0 0.0); print_endline ""
