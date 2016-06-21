2bf55f8cde3c9eeaa98ce3c5865f85eeffba9472
Yanael Barbier <ybarbier@student.42.fr>
2016-06-21 21:17:03 +0200

    Update test d05 ex01 ft_ref

--- d05/ex01/ft_ref.ml
+++ d05/ex01/ft_ref.ml
@@ -6,7 +6,7 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/20 17:42:14 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/20 22:34:53 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/21 21:15:13 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -20,3 +20,11 @@ let set x y = x.contents <- y
 
 let bind x f = return (get (f (get x)));;
 
+
+let () =
+    let n = return 21 in
+    print_int (get n); print_endline "";
+    set n 42;
+    print_int (get n); print_endline "";
+    let n_new = bind n (fun x -> (return ((string_of_int x) ^ " Bornt2Code"))) in
+    print_string (get n_new); print_endline ""
