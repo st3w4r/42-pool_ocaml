34bd5a6d9b45bd96ac339b9d1ff6af244d5d80a4
Yanael Barbier <ybarbier@student.42.fr>
2016-06-23 12:15:21 +0200

    Draw TARDIS

--- d07/ex01/doctor.ml
+++ d07/ex01/doctor.ml
@@ -6,7 +6,7 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/22 16:46:49 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/22 19:40:19 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/23 12:14:22 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -21,10 +21,28 @@ class doctor name age people =
         method to_string = _name ^ " " ^ (string_of_int _age) ^ " years old " ^
         (string_of_int _hp) ^ " HP " ^ (_sidekick#to_string)
         method talk = print_endline "Hi! I’m the Doctor!"
-        method travel_in_time (start:int) (arrival:int) = (
-            print_endline "TARDIS"
-        )
         method use_sonic_screwdriver = print_endline "Whiiiiwhiiiwhiii Whiiiiwhiiiwhiii Whiiiiwhiiiwhiii"
         method private regenerate = _hp <- 100
+        method travel_in_time (start:int) (arrival:int) = (
+            print_endline "        ___";
+            print_endline "_______(_@_)_______";
+            print_endline "| POLICE      BOX |";
+            print_endline "|_________________|";
+            print_endline " | _____ | _____ |";
+            print_endline " | |###| | |###| |";
+            print_endline " | |###| | |###| |";
+            print_endline " | _____ | _____ |";
+            print_endline " | || || | || || |";
+            print_endline " | ||_|| | ||_|| |";
+            print_endline " | _____ |$_____ |";
+            print_endline " | || || | || || |";
+            print_endline " | ||_|| | ||_|| |";
+            print_endline " | _____ | _____ |";
+            print_endline " | || || | || || |";
+            print_endline " | ||_|| | ||_|| |";
+            print_endline " |       |       |";
+            print_endline " *****************"
+        )
+
     end
 
