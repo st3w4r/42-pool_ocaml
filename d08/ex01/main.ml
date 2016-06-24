93b93afb1a9b8d5c910f1b4c393fa573693da679
Yanael Barbier <ybarbier@student.42.fr>
2016-06-24 18:10:12 +0200

    Add molecule

--- d08/ex01/main.ml
+++ d08/ex01/main.ml
@@ -6,7 +6,7 @@
 (*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2016/06/24 11:39:19 by ybarbier          #+#    #+#             *)
-(*   Updated: 2016/06/24 17:47:00 by ybarbier         ###   ########.fr       *)
+(*   Updated: 2016/06/24 18:09:02 by ybarbier         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
@@ -35,6 +35,33 @@ let () =
     print_string co2#to_string;
     print_string "Equals: ";
     if (co2#equals co22) then print_endline "True" else print_endline "False";
+
+    let hcn = new Hydrogen_cyanide.hydrogen_cyanide in
+    let hcn2 = new Hydrogen_cyanide.hydrogen_cyanide in
+    print_endline hcn#name;
+    print_endline hcn#formula;
+    print_string hcn#to_string;
+    print_string "Equals: ";
+    if (hcn#equals hcn2) then print_endline "True" else print_endline "False";
+
+
+    let c2h4 = new Ethylene.ethylene in
+    let c2h42 = new Ethylene.ethylene in
+    print_endline c2h4#name;
+    print_endline c2h4#formula;
+    print_string c2h4#to_string;
+    print_string "Equals: ";
+    if (c2h4#equals c2h42) then print_endline "True" else print_endline "False";
+
+
+    let ch4 = new Methane.methane in
+    let ch42 = new Methane.methane in
+    print_endline ch4#name;
+    print_endline ch4#formula;
+    print_string ch4#to_string;
+    print_string "Equals: ";
+    if (ch4#equals ch42) then print_endline "True" else print_endline "False";
+
 
     if (co2#equals h2o) then print_endline "True" else print_endline "False";
     if (tnt#equals co2) then print_endline "True" else print_endline "False";
---
+++ d08/ex01/methane.ml
@@ -0,0 +1,73 @@
+(* ************************************************************************** *)
+(*                                                                            *)
+(*                                                        :::      ::::::::   *)
+(*   trinitrotoluene.ml                                 :+:      :+:    :+:   *)
+(*                                                    +:+ +:+         +:+     *)
+(*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        *)
+(*                                                +#+#+#+#+#+   +#+           *)
+(*   Created: 2016/06/24 13:29:37 by ybarbier          #+#    #+#             *)
+(*   Updated: 2016/06/24 18:04:23 by ybarbier         ###   ########.fr       *)
+(*                                                                            *)
+(* ************************************************************************** *)
+
+let rec encode lst = match lst with
+| hd::tl -> (
+    let rec loop lst el c acc = match lst with
+    | hd::tl -> if (hd#equals el) then loop tl hd (c+1) acc else loop tl hd 1 (acc @ [(c, el)])
+    | [] -> acc @ [(c, el)]
+    in loop lst hd 0 []
+    )
+| [] -> []
+
+let hill atoms =
+    let rec string_list_tuple_atom lst acc = match lst with
+    | hd::tl -> (
+        let (n, x) = hd in
+        string_list_tuple_atom tl (acc ^ (x#symbole ^ (if n = 1 then "" else string_of_int n)))
+    )
+    | [] -> acc
+    in string_list_tuple_atom atoms ""
+
+let desc atoms =
+    let rec string_list_tuple_atom lst acc = match lst with
+    | hd::tl -> (
+        let (n, x) = hd in
+        string_list_tuple_atom tl (acc ^ (string_of_int n ^ " - "^ x#to_string ^ "\n"))
+    )
+    | [] -> acc
+    in string_list_tuple_atom atoms ""
+
+let priority_order atom1 atom2 =
+    let s1 = atom1#symbole in
+    let s2 = atom2#symbole in
+    if (String.compare s1 "C") = 0 then (-1)
+    else if (String.compare s2 "C") = 0 then 1
+    else if (String.compare s1 "H") = 0 then (-1)
+    else if (String.compare s2 "H") = 0 then 1
+    else String.compare s1 s2
+
+class methane =
+    object (self)
+        inherit Molecule.molecule
+        val atoms : Atom.atom list = [
+            new Carbon.carbon;
+            new Hydrogen.hydrogen;
+            new Hydrogen.hydrogen;
+            new Hydrogen.hydrogen;
+            new Hydrogen.hydrogen
+        ]
+
+        method name = "Methane"
+        method formula =
+            begin
+                let sorted_list = List.sort priority_order  atoms in
+                let atom_tuple_lst = encode sorted_list in
+                hill atom_tuple_lst
+            end
+        method to_string =
+            begin
+                let atom_tuple_lst = encode atoms in
+                desc atom_tuple_lst
+            end
+        method equals (m : Molecule.molecule) = (m#formula = self#formula)
+    end
